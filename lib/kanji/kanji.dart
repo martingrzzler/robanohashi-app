63168b9382f2f5da91811ca5b706eb537e3f81b2
Martin Gressler <martin.gressler@code.berlin>
2023-03-01 15:43:35 +0100

    added basic kanji view

--- lib/kanji/kanji.dart
+++ lib/kanji/kanji.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:jovial_svg/jovial_svg.dart';
 import 'package:robanohashi/common/colors.dart';
 import 'package:robanohashi/common/subject_card.dart';
 import 'package:robanohashi/kanji/api.dart';
+import 'package:robanohashi/search/api.dart';
+
+import 'readings.dart';
 
 class KanjiViewArgs {
   KanjiViewArgs({required this.id});
@@ -55,23 +59,116 @@ class _KanjiViewState extends State<KanjiView> {
               ],
             ));
           }
+
+          final data = snapshot.data as Kanji;
+
           return Padding(
             padding: const EdgeInsets.all(10.0),
-            child: Column(children: [
-              Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
+            child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
                 children: [
-                  SubjectCard(
-                      color: getSubjectBackgroundColor("kanji"),
-                      child: Text(snapshot.data!.characters,
-                          style: const TextStyle(
-                              fontSize: 50, color: Colors.white))),
-                ],
-              ),
-            ]),
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      SubjectCard(
+                          color: getSubjectBackgroundColor("kanji"),
+                          child: Text(data.characters,
+                              style: const TextStyle(
+                                  fontSize: 60, color: Colors.white))),
+                      const SizedBox(width: 10),
+                      Expanded(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                                data.meanings
+                                    .firstWhere((element) => element.primary)
+                                    .meaning,
+                                style: TextStyle(
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.grey[700])),
+                            const SizedBox(height: 5),
+                            Text(
+                                data.meanings
+                                    .where((element) => !element.primary)
+                                    .map((e) => e.meaning)
+                                    .join(', '),
+                                style: TextStyle(
+                                    fontSize: 18, color: Colors.grey[700])),
+                            const SizedBox(height: 13),
+                            KanjiReadings(readings: data.readings)
+                          ],
+                        ),
+                      ),
+                      // 3. Primary reading
+                      // 4. Other readings
+                    ],
+                  ),
+                  const SizedBox(height: 10),
+                  Divider(),
+                  RadicalComposition(
+                    radicals: data.componentSubjects,
+                  ),
+                ]),
           );
         },
       ),
     );
+  }
+}
+
+class RadicalComposition extends StatelessWidget {
+  const RadicalComposition({
+    super.key,
+    required this.radicals,
+  });
+
+  final List<SubjectPreview> radicals;
+
+  Widget _buildRadical(SubjectPreview radical) {
+    return Column(
+      children: [
+        SubjectCard(
+          color: getSubjectBackgroundColor('radical'),
+          child: radical.characters != ""
+              ? Text(
+                  radical.characters,
+                  style: const TextStyle(
+                    color: Colors.white,
+                    fontSize: 30.0,
+                  ),
+                )
+              : SizedBox(
+                  width: 30,
+                  height: 30,
+                  child: ScalableImageWidget(
+                      si: ScalableImage.fromSvgString(radical.characterImage)),
+                ),
+        ),
+        const SizedBox(height: 2),
+        Text(
+          radical.meanings.first,
+          style: TextStyle(color: Colors.grey[600], fontSize: 15.0),
+        )
+      ],
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+        scrollDirection: Axis.horizontal,
+        child: Wrap(
+          crossAxisAlignment: WrapCrossAlignment.center,
+          spacing: 10,
+          children:
+              List.generate(radicals.length + radicals.length - 1, (index) {
+            return (index % 2 == 0)
+                ? _buildRadical(radicals[index ~/ 2])
+                : Text('+',
+                    style: TextStyle(fontSize: 25, color: Colors.grey[700]));
+          }),
+        ));
   }
 }
---
+++ lib/kanji/readings.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+import 'api.dart';
+
+class KanjiReadings extends StatelessWidget {
+  const KanjiReadings({
+    super.key,
+    required this.readings,
+  });
+
+  final List<KanjiReading> readings;
+
+  KanjiReading? _findByType(String type) {
+    final res = readings.where((element) => element.type == type);
+    return res.isNotEmpty ? res.first : null;
+  }
+
+  Widget _buildReading(String type, String label) {
+    final reading = _findByType(type);
+
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Text(
+          reading?.reading ?? 'None',
+          style: TextStyle(
+              fontSize: 18,
+              fontWeight: reading != null ? FontWeight.bold : FontWeight.normal,
+              color: reading != null ? Colors.grey[700] : Colors.grey[400]),
+        ),
+        Text(label, style: TextStyle(color: Colors.grey[400])),
+      ],
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.end,
+      children: [
+        _buildReading("onyomi", "On'yomi"),
+        _buildReading('kunyomi', 'Kun\'yomi'),
+        _buildReading('nanori', 'Nanori'),
+      ],
+    );
+  }
+}
